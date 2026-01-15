123edeca70d102cadfce91c4690b1d87ece031f8
gkhlevtov <gkhlevtov@mail.ru>
2026-01-15 20:31:24 +0300

    [lab1 t3] Redesigned the testbench using mailboxes

--- lab1/task3/tb/priority_encoder_tb.sv
+++ lab1/task3/tb/priority_encoder_tb.sv
@@ -2,6 +2,9 @@ module priority_encoder_tb;
   // Data size
   parameter WIDTH = 8;
 
+  // Number of random tests 
+  parameter N     = 50;
+
   // DUT inputs and output
   bit clk;
   bit srst;
@@ -13,13 +16,17 @@ module priority_encoder_tb;
   logic [WIDTH-1:0] data_right_o;
   logic             data_val_o;
 
-  // Recieved data
-  logic [WIDTH-1:0] rx_left;
-  logic [WIDTH-1:0] rx_right;
-  logic             rx_valid;
+  // Tests counters and flag
+  int total_tests;
+  int expected_tests;
+  int checked_tests;
+  bit tests_done;
 
   // Simulation success flag
   bit pass_flag;
+
+  // Reset flag
+  bit rst_done;
 
   typedef struct packed {
     logic [WIDTH-1:0] d_i;
@@ -29,7 +36,11 @@ module priority_encoder_tb;
     logic [WIDTH-1:0] exp_r;
   } task_input;
 
-  task_input test_data;
+  // Mailbox for generated data
+  mailbox #( task_input ) gen_mbx = new();
+
+  // Mailbox for expected data
+  mailbox #( task_input ) exp_mbx = new();
 
   initial
     forever
@@ -41,16 +52,15 @@ module priority_encoder_tb;
       srst       = '0;
       data_i     = '0;
       data_val_i = '0;
-      test_data  = '0;
-      rx_left    = '0;
-      rx_right   = '0;
       pass_flag  = '1;
+      tests_done = '0;
 
       @( posedge clk );
       srst       = '1;
 
       @( posedge clk );
       srst       = '0;
+      rst_done   = '1;
     end
   
   priority_encoder #(
@@ -90,87 +100,12 @@ module priority_encoder_tb;
     return generated_data;
   endfunction
 
-  task automatic reset();
-    begin
-      data_i     = '0;
-      data_val_i = '0;
-      srst       = '0;
-
-      @( posedge clk );
-      srst       = '1;
-
-      @( posedge clk );
-      srst       = '0;
-    end
-  endtask
-
-  task automatic send_data( logic [WIDTH-1:0] tx, bit valid );
-    begin
-      data_i     = tx;
-      data_val_i = valid;
-      @( posedge clk );
-      data_val_i = 1'b0;
-    end
-  endtask
-
-  task automatic receive_data(
-    output logic [WIDTH-1:0] rx_left,
-    output logic [WIDTH-1:0] rx_right,
-    output bit               rx_valid
+  task automatic generate_tests(
+    int random_n,
+    mailbox #( task_input ) mbx
   );
     begin
-      int wait_cycles;
-
-      rx_left     = '0;
-      rx_right    = '0;
-      rx_valid    = 1'b0;
-      wait_cycles = 0;
-
-      while( wait_cycles < 2 )
-        begin
-          @( posedge clk );
-          if( data_val_o )
-            begin
-              rx_left  = data_left_o;
-              rx_right = data_right_o;
-              rx_valid = 1'b1;
-              return;
-            end
-          wait_cycles++;
-        end
-    end
-  endtask
-
-  task automatic run_one_check( task_input test_data );
-    begin
-      reset();
-      fork
-        send_data(test_data.d_i, test_data.d_v_i);
-        receive_data(rx_left, rx_right, rx_valid);
-      join
-
-      if( rx_valid != test_data.exp_v )
-        begin
-          $error( "Valid mismatch:\nexp=%b\ngot=%b\n", test_data.exp_v, rx_valid);
-          pass_flag = 1'b0;
-        end
-      else if( rx_valid )
-        begin
-          if( ( rx_left != test_data.exp_l ) || ( rx_right != test_data.exp_r ) )
-            begin
-              $error( "Data mismatch:\ninp_d=%b\nexp_l=%b\ngot_l=%b\nexp_r=%b\ngot_r=%b\n",
-                      test_data.d_i, test_data.exp_l, rx_left, test_data.exp_r, rx_right );
-              pass_flag = 1'b0;
-            end
-        end
-    end
-  endtask
-
-  initial
-    begin
-      $display( "Simulation start" );
-      
-      #20;
+      task_input test_data;
 
       // Zeroes test
       test_data.d_i   = '0;
@@ -178,7 +113,9 @@ module priority_encoder_tb;
       test_data.exp_v = 1'b1;
       test_data.exp_l = '0;
       test_data.exp_r = '0;
-      run_one_check(test_data);
+      mbx.put(test_data);
+      total_tests++;
+      expected_tests++;
 
       // Ones test
       test_data.d_i   = '1;
@@ -186,7 +123,9 @@ module priority_encoder_tb;
       test_data.exp_v = 1'b1;
       test_data.exp_l = ( 1 << ( WIDTH - 1 ) );
       test_data.exp_r = 1;
-      run_one_check(test_data);
+      mbx.put(test_data);
+      total_tests++;
+      expected_tests++;
 
       // Invalid input data test
       test_data.d_i   = $urandom();
@@ -194,7 +133,8 @@ module priority_encoder_tb;
       test_data.exp_v = 1'b0;
       test_data.exp_l = '0;
       test_data.exp_r = '0;
-      run_one_check(test_data);
+      mbx.put(test_data);
+      total_tests++;
 
       // Single bit test
       test_data.d_i   = ( 1 << ( WIDTH / 2 ) );
@@ -202,23 +142,106 @@ module priority_encoder_tb;
       test_data.exp_v = 1'b1;
       test_data.exp_l = test_data.d_i;
       test_data.exp_r = test_data.d_i;
-      run_one_check(test_data);
+      mbx.put(test_data);
+      total_tests++;
+      expected_tests++;
 
       // Random tests
-      repeat(50)
+      repeat( random_n )
         begin
           test_data = generate_input();
-          run_one_check(test_data);
+          mbx.put(test_data);
+
+          total_tests++;
+          if ( test_data.exp_v )
+            expected_tests++;
         end
       
-      #20;
+      $display("Generator: %0d test inputs queued", total_tests);
+    end
+  endtask
+
+  task automatic send_inputs(
+    mailbox #( task_input ) gen_mbx,
+    mailbox #( task_input ) exp_mbx
+  );
+    begin
+      task_input tx_data;
+
+      forever
+        begin
+          gen_mbx.get( tx_data );
+
+          data_i     <= tx_data.d_i;
+          data_val_i <= tx_data.d_v_i;
+          @( posedge clk );
+          data_val_i <= 1'b0;
+
+          if( tx_data.exp_v )
+            exp_mbx.put( tx_data );
+        end
+    end
+  endtask
+
+  task automatic verify_outputs( mailbox #( task_input ) exp_mbx );
+    begin
+      task_input exp_data;
+
+      forever
+        begin
+          @( posedge clk );
+
+          if( data_val_o )
+            begin
+              if( !exp_mbx.try_get( exp_data ) )
+                begin
+                  $error( "Unexpected valid output!\n" );
+                  pass_flag = 1'b0;
+                end
+              else
+                begin
+                  if( ( data_left_o != exp_data.exp_l ) || ( data_right_o != exp_data.exp_r ) )
+                    begin
+                      $error( "Data mismatch:\ninp_d=%b\nexp_l=%b\ngot_l=%b\nexp_r=%b\ngot_r=%b\n",
+                        exp_data.d_i,
+                        exp_data.exp_l, data_left_o,
+                        exp_data.exp_r, data_right_o
+                      );
+                      pass_flag = 1'b0;
+                    end
+                end
+
+              checked_tests++;
+              if( checked_tests == expected_tests )
+                tests_done = 1'b1;
+            end
+        end
+    end
+  endtask
+
+  initial
+    begin
+      wait( rst_done );
+
+      $display( "Simulation start" );
+      
+      generate_tests( N, gen_mbx );
+
+      fork
+        send_inputs( gen_mbx, exp_mbx );
+        verify_outputs( exp_mbx );
+      join_none
+
+      wait( tests_done );
 
       if( pass_flag )
         $display( "\nTEST PASSED\n" );
       else
         $display( "\nTEST FAILED\n" );
-    
+
+      #20;
+
       $display( "Simulation end" );
-      $stop();
+      $finish;
     end
 endmodule
